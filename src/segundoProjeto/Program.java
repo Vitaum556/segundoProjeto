007fd1ef271eda9565b627aa2c0955cc4374f2a9
Victor <victor.mateus.silva@hotmail.com>
2022-11-23 00:35:57 -0300

    desenvolvido o codigo

--- src/segundoProjeto/Program.java
+++ src/segundoProjeto/Program.java
@@ -1,10 +1,32 @@
 package segundoProjeto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		System.out.println("segundo projeto");
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("escreva tres numeros");
+		System.out.print("a: ");
+		int a = sc.nextInt();
+		System.out.print("b: ");
+		int b = sc.nextInt();
+		System.out.print("c: ");
+		int c = sc.nextInt();
+		
+		System.out.println(a + " - " + b + " - " + c);
 		
 
+		
+		
+		sc.close();
+		
+		
 	}
 
 }
